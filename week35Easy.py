f8d7055202c1813b7db4f50e22191cd26bfc3b5c
Tellysone87 <walterdtrt@aol.com>
2023-07-25 23:05:58 -0400

    week 35 challenge has changes. connot figure out the negative mean formula

--- week35Easy.py
+++ week35Easy.py
@@ -9,10 +9,38 @@
 def mean(num_list):
     """ function that takes a list of numbers and calculates the geometric mean."""
 
-    mean = 0  # set the mean variable to zero, it will be updated later in the function.]
+    mean = 0  # set the mean variable to zero, it will be updated later in the function.
     multiply = 1  # we will start by multiplying the first num by 1
     list_length = len(num_list)  # tells us how many number are in the list.
+    i = 0
 
+    if list_length == 0:
+        return "No numbers provided"
+
+    # # steps is negative numbers are present
+    # for number in num_list: # for number in list
+    #     if number < 0: # if there is a negative number
+    #         while i <= list_length - 1: # starting at the first index
+    #             num_list[i] = num_list[i]/100 # we multiply the number by 100 to get decimal
+    #             print(num_list[i])
+    #             if num_list[i] > 0: # next we add 1 if the number is positive or subtract 1 if the number is negative
+    #                 num_list[i] = 1 + num_list[i]
+    #                 print(num_list[i])
+    #                 multiply = multiply * num_list[i]
+                    
+    #             elif num_list[i]  < 0:
+    #                num_list[i]  = 1 + num_list[i]
+    #                print(num_list[i])
+    #                multiply = multiply *  num_list[i]
+    #             i+=1
+    #         print(multiply)
+    #         root_of = 1/list_length
+    #         mean = pow(multiply, root_of) 
+    #         print(mean -1)
+    #         print(mean*100)
+    #         return mean * 100
+                
+    # Condition for positive numbers          
     for number in num_list:  # loop through list and multiply by the previous number
         multiply = multiply * number
 
@@ -28,5 +56,8 @@ def main():
 
     test2 = mean([10, 51.2, 8])
     print(test2)
+
+    test3 = mean([])
+    print(test3)
 
 main()
