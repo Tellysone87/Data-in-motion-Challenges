23310c3d88b41d906dab4f8bff797174a6f3b9c1
Tellysone87 <walterdtrt@aol.com>
2023-07-26 22:43:41 -0400

    progress on week 36 expert challenge

--- week35Medium.py
+++ week35Medium.py
@@ -2,32 +2,32 @@
 # Author: Shantel Williams
 
 # Medium:
-# 1. Given the following string, input_string, write a Python function that takes a string and removes all punctuation characters from it.
-# input_string = “Hello, World! Do you think pineapples belong on pizza?”
 # 2. Given the following string, input_string, write a Python function that takes a string and removes all stop words (e.g. “the”, “a”, “an”) from it.
 # input_string = “The quick brown fox jumps over the lazy dog”
 
-# After research, I was able to find a stop word library that could help me with this function. Here is the documentation link - https://pypi.org/project/nltk/
+# After research, I was able to find a stop word module that could help me with this function. Here is the documentation link - https://pypi.org/project/nltk/
 # i then installed it for use with - python3 -m pip install nltk
 
-import nltk # Natural language Toolkit - https://www.nltk.org/   ---- https://www.nltk.org/api/nltk.corpus.html
-from nltk.corpus import stopwords # provides a list of stop words
+# Natural language Toolkit designed to work with human language data - https://www.nltk.org/   ----   https://www.nltk.org/api/nltk.corpus.html
+# provides a list of stop words. A stop word is a commonly used word that a search engine has been programmed to ignore. Straightforward - useless data/words. 
+from nltk.corpus import stopwords
 
 def string_manipulation(string_input):
     """ Function that takes a string and removes all stop words (e.g. “the”, “a”, “an”) from it."""
+
     index = 0 # variable to track our index 
     new_string = [] # I am using this list to store the words that are not stop words
 
-    # nltk.download('stopwords')
+    # nltk.download('stopwords') # you have to download the list once in the terminal
     stop_words = set(stopwords.words('english')) # grabbing the english stop words from the nltk module
 
     if not string_input: # condition if empty string is passed
         return "Empty string was provided"
     
-    words = string_input.split(" ") # created a list from seperating each word in the string. This is for comparing. I am splitting on " "
+    words = string_input.split(" ") # created a list from seperating each word in the string. This is for comparing. I am splitting on " ".
     
     while index < len(words): # looping through the word list
-        if words[index].lower() not in stop_words: # if the lower cases form of the index is not in the downloaded nltk list
+        if words[index].lower() not in stop_words: # if the lower case form of the index is not in the downloaded nltk list
             new_string.append(words[index]) # I am adding it to the new string list
         index += 1 # increment to avoid infinite while loop and move up the word list index
 
--- week36Expert.py
+++ week36Expert.py
@@ -1,4 +1,4 @@
-# Date: 7/25/2023
+# Date: 7/27/2023
 # Author: Shantel Williams
 
 # Expert: Create a function that returns the product of two integers. This process of multiplication can be achieved via repetitive addition, thus, the process can be done recursively.
@@ -14,23 +14,39 @@
 # Notes:
 # You’re expected to solve this challenge using a recursive approach
 
-
-
-def multiply(int1,int2, i = 0):
+def multiply(int1,int2):
     """ Function that returns the product of two integers."""
-    add = int1
 
-    while i < int2:
-        print(add)
-        i+=1
-        int1+=int1
-        multiply(int1,int2,i)
-       
-    return add
+    if int1 == 0 or int2 == 0: # condition if any of the numbers are zero
+        return 0
+    
+    if int2 > 0:
+        if int2 == 1: # base condition for positive numbers 
+            return int1
+        return int1 + multiply(int1, int2 - 1)
+        
+    if int2 < 0:
+        if int2 == -1:
+            return int1
+        return int1 + multiply(int1, int2 + 1)
 
 def main():
     test = multiply(10, 2)
+    test2 =  multiply(-51, -4)
+    test3 = multiply(3, 9)
+    test4 = multiply(-21, 5)
+    test5 = multiply(1024, 7)
+    test6 = multiply(273, -6)
+    test7 = multiply(0,10)
+    test8 = multiply(110,0)
 
     print(test)
+    print(test2)
+    print(test3)
+    print(test4)
+    print(test5)
+    print(test6)
+    print(test7)
+    print(test8)
 
 main()
