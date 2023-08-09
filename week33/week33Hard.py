2bb6feda59626e6c3bb303ce3b31362f6f94ab0a
Tellysone87 <walterdtrt@aol.com>
2023-08-09 04:45:47 -0400

    updates

--- week33/week33Hard.py
+++ week33/week33Hard.py
@@ -1,16 +1,11 @@
 # Date: 8/9/2023
 # Author: Shantel Williams
-# Hard:
-# Create a function to perform basic arithmetic operations that includes addition, subtraction, multiplication
+# Hard: Create a function to perform basic arithmetic operations that includes addition, subtraction, multiplication
 # and division on a string number (e.g. “12 + 24” or “23 – 21” or “12 // 12” or “12 * 21”).
                                  
-# Here, we have 1 followed by a space, operator followed by another space and 2. 
 # For the challenge, we are going to have only two numbers between 1 valid operator. 
 # The return value should be a number.
 # eval() is not allowed. In case of division, whenever the second number equals “0” return -1.
-
-# For example:
-# “15 // 0” ➞ -1
 
 # Examples:
 # arithmetic_operation(“12 + 12”) ➞ 24
@@ -19,16 +14,14 @@
 # arithmetic_operation(“12 // 0”) ➞ -1
 
 # Notes:
-# All the inputs are only integers.
-# The operators are * – + and //.
+# All the inputs are only integers. The operators are * – + and //.
 # Hint: Think about the single space that appears before and after the arithmetic operator.
-
 def arithmetic_operation(string_number):
     """ Function to perform basic arithmetic operations that includes addition, subtraction, multiplication and division on a string number """
     answer = 0 # set initial answer to zero
 
     seperated_string = string_number.split(" ")# split provided string into a list. Example: “12 + 12” --> ['12', '+', '12']
-    
+
     # conditions to determine operator
     if seperated_string[1] == "+":
         answer = int(seperated_string[0]) + int(seperated_string[2])
