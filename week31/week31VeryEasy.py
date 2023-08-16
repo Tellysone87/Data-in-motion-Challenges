265c857965fbe1bf1175d89c3232c265df3214ea
Tellysone87 <walterdtrt@aol.com>
2023-08-16 12:12:34 -0400

    completed week 31 very easy

--- week31/week31VeryEasy.py
+++ week31/week31VeryEasy.py
@@ -6,7 +6,8 @@
 # chickens = 2 legs
 # cows = 4 legs
 # pigs = 4 legs
-# The farmer has counted his animals and he gives you a subtotal for each species. You have to implement a function that returns the total number of legs of all the animals.
+# The farmer has counted his animals and he gives you a subtotal for each species. 
+# You have to implement a function that returns the total number of legs of all the animals.
 
 # Examples:
 # animals(2, 3, 5) ➞ 36
@@ -19,13 +20,13 @@
 # Remember that the farmer wants to know the total number of legs and not the total number of animals.
 def animals(chickens,cows,pigs):
     """ Afunction that returns the total number of legs of all the animals. """
-    total_legs  = 0 
+    total_legs  = 0 # variable to store total legs
     animal_leg_count = {"chickens": 2,"cow": 4 ,"pig": 4} # dictionary to store leg counts for designated animal
 
     #count animal legs
     total_legs += chickens * animal_leg_count.get("chickens", 0) # chickens times the stored legs value
-    total_legs += cows * animal_leg_count.get("cow", 0) # chickens times the stored legs value
-    total_legs += pigs * animal_leg_count.get("pig", 0) # chickens times the stored legs value
+    total_legs += cows * animal_leg_count.get("cow", 0) # cows times the stored legs value
+    total_legs += pigs * animal_leg_count.get("pig", 0) # pigs times the stored legs value
 
     return total_legs
 
