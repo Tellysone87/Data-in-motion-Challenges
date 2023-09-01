5a9c9ce62218a1b2be3b858b840a64596e4e5e03
Tellysone87 <walterdtrt@aol.com>
2023-09-01 04:19:30 -0400

    completed week 27

---
+++ week27/week27.py
@@ -0,0 +1,38 @@
+# Date: 8/31/2023
+# Author: Shantel Williams
+
+# Create a function that takes a list of numbers and a string and return a list of numbers as per the following rules:
+
+# “Asc” returns a sorted list in ascending order.
+# “Des” returns a sorted list in descending order.
+# “None” returns a list without any modification.
+
+# Examples:
+# asc_des_none([4, 3, 2, 1], “Asc” ) ➞ [1, 2, 3, 4]
+# asc_des_none([7, 8, 11, 66], “Des”) ➞ [66, 11, 8, 7]
+# asc_des_none([1, 2, 3, 4], “None”) ➞ [1, 2, 3, 4]
+
+def asc_des_none(num_list, sort):
+    """ function that takes a list of numbers and a string 
+    and return a list of numbers based on provided rules"""
+
+# “Asc” returns a sorted list in ascending order.
+    if sort == "Asc":
+        return sorted(num_list)
+
+# “Des” returns a sorted list in descending order.
+    if sort == "Des":
+            return sorted(num_list,reverse = True)
+
+# “None” returns a list without any modification.
+    if sort == "None":
+        return num_list
+
+def main():
+    test = asc_des_none([4, 3, 2, 1], "Asc") 
+    test2 = asc_des_none([7, 8, 11, 66], "Des")
+    test3 = asc_des_none([1, 2, 3, 4], "None")
+    print(test)
+    print(test2)
+    print(test3)
+main()
