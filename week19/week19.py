317ae95068674b389714dff5ca3ea91a1e6d7ef6
Tellysone87 <walterdtrt@aol.com>
2023-09-14 11:34:39 -0400

    completed week 19

---
+++ week19/week19.py
@@ -0,0 +1,22 @@
+# Date: 9/13/2023
+# Author: Shantel Williams
+
+# Problem: Create a function that takes a number num and returns its length.
+
+# Examples:
+# number_length(10) ➞ 2
+# number_length(5000) ➞ 4
+# number_length(0) ➞ 1
+def number_length(num):
+    return len(str(num))
+
+def main():
+    tests = [
+    number_length(10),
+    number_length(5000),
+    number_length(0)]
+
+    for test in tests:
+        print(test)
+
+main()
