af85fa7b42e0ed1a8db7341c47431cdbe5143e54
Tellysone87 <walterdtrt@aol.com>
2023-10-02 17:30:05 -0400

    week 3 completed

--- week3/week3.py
+++ week3/week3.py
@@ -1,20 +1,33 @@
  # Date: 10/9/2023
  # Author: Shantel Williams
 
-# Problem 1: Create a function that takes two arguments: the original price and the discount percentage as integers and returns the final price after the discount.
-
-
-
-
 # Problem 2: Create a function that takes the age in years and returns the age in days.
-
-
-
-
 # Problem 3: Create a function that takes an angle in radians and returns the corresponding angle in degrees rounded to one decimal place.
-
-
-
-
 # Bonus (Optional)
 # Create a function, get_days, that takes two dates and returns the number of days between the first and second date
+
+
+# Problem 1: Create a function that takes two arguments: the original price and the discount percentage as integers and returns the final price after the discount.
+
+# TestCases:
+# function{100,10} --> $90.00
+# function{50,15} --> $42.50
+def total_price(num1,num2):
+    percentage = float(num2 / 100)
+
+    total_discount = float(percentage * num1)
+    
+    return f" ${format(num1 - total_discount,'.2f')}"
+
+
+
+def main():
+    test = [
+        total_price(100,10),
+        total_price(50,15)
+    ]
+
+    for t in test:
+        print(t)
+
+main()
