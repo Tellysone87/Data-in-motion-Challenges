a815b5cd53bd60bed7e89ea0028a0372bbfbe003
Tellysone87 <walterdtrt@aol.com>
2023-10-02 13:55:28 -0400

    week 6 completed

---
+++ week3/week3.py
@@ -0,0 +1,20 @@
+ # Date: 10/9/2023
+ # Author: Shantel Williams
+
+# Problem 1: Create a function that takes two arguments: the original price and the discount percentage as integers and returns the final price after the discount.
+
+
+
+
+# Problem 2: Create a function that takes the age in years and returns the age in days.
+
+
+
+
+# Problem 3: Create a function that takes an angle in radians and returns the corresponding angle in degrees rounded to one decimal place.
+
+
+
+
+# Bonus (Optional)
+# Create a function, get_days, that takes two dates and returns the number of days between the first and second date
---
+++ week5/week5.py
@@ -0,0 +1,5 @@
+ # Date: 10/5/2023
+ # Author: Shantel Williams
+
+# Create a function that takes in a current mood and return a sentence in the following format: 
+# “Today, I am feeling {mood}”. However, if no argument is passed, return “Today, I am feeling neutral”.
