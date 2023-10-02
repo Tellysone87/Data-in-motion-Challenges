913ffe34a719d0b5a1efd6e20b72f92b1b652ce2
Tellysone87 <walterdtrt@aol.com>
2023-10-02 15:04:56 -0400

    week 5 completed

--- week5/week5.py
+++ week5/week5.py
@@ -3,3 +3,19 @@
 
 # Create a function that takes in a current mood and return a sentence in the following format: 
 # “Today, I am feeling {mood}”. However, if no argument is passed, return “Today, I am feeling neutral”.
+
+def feeling(mood="neutral"): # sets default value
+    return f"Today, I am feeling {mood}"
+   
+
+def main():
+    test = [
+        feeling(),
+        feeling("Happy"),
+        feeling("Excited")
+    ]
+
+    for t in test:
+        print(t)
+
+main()
