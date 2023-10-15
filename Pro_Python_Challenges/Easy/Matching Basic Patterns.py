43b8a01069212f83895579cf31e4f3d8327b2192
Tellysone87 <walterdtrt@aol.com>
2023-10-15 18:58:46 -0400

    pro python challenges

--- Pro_Python_Challenges/Easy/Matching Basic Patterns.py
+++
@@ -1,4 +0,0 @@
-# Write a Python function that accepts a string and checks if the string starts with “Hello”. If it does, return True. 
-# If not, return False. Use regular expressions in your solution.
-
-# test_string = ‘Hello World’
