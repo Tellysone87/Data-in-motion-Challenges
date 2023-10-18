bbc60b802aefe15bdd580ef58b47769c840a3828
Tellysone87 <walterdtrt@aol.com>
2023-10-18 13:50:47 -0400

    pro python challenges

---
+++ Pro_Python_Challenges/Easy/FilterPositiveNumbers.py
@@ -0,0 +1,7 @@
+# 10/20/2023
+# Shantel Williams
+
+# Using list comprehension, write a function named filter_positive_numbers that takes a list of integers as input 
+# and returns a new list containing only the positive numbers from the input list.
+
+# numbers = [-2, -1, 0, 1, 2]
