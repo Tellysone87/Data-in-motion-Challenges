334725e6d3307d99ae0faca60a33e61466206698
Tellysone87 <walterdtrt@aol.com>
2023-12-04 17:20:27 -0500

    completed Week 9-25 Easy

---
+++ weeklyPythonChallenges/September25/Easy.py
@@ -0,0 +1,19 @@
+# Shantel Williams
+# 12/5/2023
+
+# Easy Challenge: Duplicate Detector
+# Write a function named has_duplicates(lst) that accepts a list and returns True if the list contains any duplicates, otherwise False.
+
+# list1 = [1, 2, 3, 4, 5]
+# list2 = [5, 4, 3, 2, 5]
+# list3 = ['a', 'b', 'c', 'a']
+
+def has_duplicates(lst):
+    pass
+
+
+def main():
+    pass
+
+
+main()
