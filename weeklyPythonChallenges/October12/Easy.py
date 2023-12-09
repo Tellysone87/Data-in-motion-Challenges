f8766350f4221d1bb6bf36dfb2587a9f4bb5b838
Tellysone87 <walterdtrt@aol.com>
2023-12-09 16:23:47 -0500

    starting 10-12 Easy challenge - completed

---
+++ weeklyPythonChallenges/October12/Easy.py
@@ -0,0 +1,27 @@
+# Shantel Williams
+# 12/9/2023
+
+# Easy Challenge: Dictionary Value Doubler
+# You are given a dictionary where the keys are strings and the values are numbers. 
+# Write a function double_values(d) that doubles each value in the dictionary and returns the modified dictionary.
+
+# sample_dict = {
+#     'a': 5,
+#     'b': 10,
+#     'c': 15
+# }
+
+def double_values(provided_dict):
+    pass
+    
+
+def main():
+    sample_dict = {
+    'a': 5,
+    'b': 10,
+    'c': 15
+}
+    
+    print(double_values(sample_dict))
+
+main()
