e8a9de18974a30ae68813a96273e0daf11aad32d
Tellysone87 <walterdtrt@aol.com>
2023-12-10 17:36:13 -0500

    starting 10-30 Medium challenge - completed

--- weeklyPythonChallenges/October30/Medium.py
+++ weeklyPythonChallenges/October30/Medium.py
@@ -1,9 +1,9 @@
 # Shantel Williams
 # 12/15/2023
-
-
 # Medium: Sales above Average
-# For sales analysts, it’s often useful to identify which salespeople or products are performing above average. Given a dictionary of product sales, create a function that identifies products whose sales are above the average sales for all products.
+
+# For sales analysts, it’s often useful to identify which salespeople or products are performing above average. 
+# Given a dictionary of product sales, create a function that identifies products whose sales are above the average sales for all products.
 
 # product_sales = {
 #     "Laptop": 1200,
@@ -14,3 +14,18 @@
 # }
 
 # https://d-i-motion.com/topics/python-challenge-week-8/
+
+def above_avg_sales(sale_dict):
+    above = lambda key : sale_dict[key] > sum(sale_dict.values()) // len(sale_dict)
+    return list(filter(above,sale_dict))
+
+def main():
+    product_sales = {
+    "Laptop": 1200,
+    "Smartphone": 700,
+    "Tablet": 500,
+    "Headphones": 300,
+    "Smartwatch": 400,
+}
+    print(above_avg_sales(product_sales))
+main()
