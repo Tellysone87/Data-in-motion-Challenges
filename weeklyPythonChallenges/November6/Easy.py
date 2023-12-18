af6315a8c10a3b40e1f79674539630e347517fd8
Tellysone87 <walterdtrt@aol.com>
2023-12-18 13:09:18 -0500

    November 6 Easy challenge

---
+++ weeklyPythonChallenges/November6/Easy.py
@@ -0,0 +1,18 @@
+# Shantel Williams
+# 12/18/2023
+
+# Problem Statement: You have a list of ratings (from 1 to 5) given by customers for a product. Write a function to calculate the average rating.
+
+# ratings = ['5', '3', '4', '2', '5', '5', '1']
+
+def average_rating():
+    pass
+
+def main():
+    pass
+
+main()
+
+
+
+
