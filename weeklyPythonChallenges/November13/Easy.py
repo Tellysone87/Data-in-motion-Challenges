4cf12f5cd7d83ffe6379c0b5867aabf43834bf36
Tellysone87 <walterdtrt@aol.com>
2023-12-24 15:41:23 -0500

    Setting up the last challenges in weekly course

---
+++ weeklyPythonChallenges/November13/Easy.py
@@ -0,0 +1,9 @@
+# Shantel Williams
+# 12/26/2023
+
+# Problem Statement: Given a list of monthly sales figures for a year, calculate the month-to-month sales growth in percentage.
+
+# Expected Task: Create a function that calculates the sales growth percentage for each month compared to the previous month 
+# and returns a list of these percentages.
+
+# monthly_sales = [1200, 1150, 1300, 1400, 1350, 1500, 1550, 1600, 1650, 1700, 1750, 1800]
---
+++ weeklyPythonChallenges/November13/Medium.py
@@ -0,0 +1,18 @@
+# Shantel Williams
+# 12/29/2023
+
+# Problem Statement: You have a dataset containing customer IDs and their purchase amounts in various categories. Segment the customers into three categories: ‘High’, ‘Medium’, and ‘Low’ based on their total purchase amounts.
+
+# Segmentation Logic:
+
+# High: Total purchases > 1500
+# Medium: Total purchases between 500 and 1500
+# Low: Total purchases < 500
+# Expected Task: Write a function to categorize each customer based on their total purchase amount and return a dictionary with customer IDs and their respective segments.
+
+# purchases = [
+#     {'customer_id': 1, 'electronics': 200, 'furniture': 700, 'stationery': 100},
+#     {'customer_id': 2, 'electronics': 300, 'furniture': 300, 'stationery': 300},
+#     {'customer_id': 3, 'electronics': 100, 'furniture': 100, 'stationery': 50},
+#     {'customer_id': 4, 'electronics': 800, 'furniture': 1000, 'stationery': 200}
+# ]
