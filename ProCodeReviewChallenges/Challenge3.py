ee0ef2d1c1df53cf36255f31b0f9f27dc3c0f563
Tellysone87 <walterdtrt@aol.com>
2024-01-01 14:16:48 -0500

    starting new challenges

--- ProCodeReviewChallenges/Challenge3.py
+++ ProCodeReviewChallenges/Challenge3.py
@@ -1,2 +1,13 @@
 # 1/9/2024
 # Shantel Williams
+
+# Problem:
+# This code aims to swap the keys and values in a dictionary. But it doesn’t work as expected. Identify and fix the error.
+# https://d-i-motion.com/lessons/challenge-3/
+
+def swap_dict(dictionary):
+    new_dict = {value: key for key, value in dictionary}
+    return new_dict
+
+my_dict = {1: 'a', 2: 'b', 3: 'c'}
+print(swap_dict(my_dict)) # Expected: {'a': 1, 'b': 2, 'c': 3}
--- ProCodeReviewChallenges/Challenge4.py
+++ ProCodeReviewChallenges/Challenge4.py
@@ -1,2 +1,15 @@
 # 1/12/2024
 # Shantel Williams
+
+# The code is supposed to handle a division by zero exception, but it’s still throwing an error.
+# https://d-i-motion.com/lessons/challenge-4/
+
+def divide_numbers(num1, num2):
+    try:
+        result = num1 / num2
+    except ZeroDivisionError:
+        print('Cannot divide by zero')
+    finally:
+        return result
+
+print(divide_numbers(4, 0))
