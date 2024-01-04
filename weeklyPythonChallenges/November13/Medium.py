0857cb98b02bda6508be4fde140d697a9a531b04
Tellysone87 <walterdtrt@aol.com>
2024-01-04 16:43:30 -0500

    fixed and updates challenges with input

--- weeklyPythonChallenges/November13/Medium.py
+++ weeklyPythonChallenges/November13/Medium.py
@@ -32,9 +32,9 @@ def segment_customers(purchase_list):
 
         if total(cust) > 1500:
             category[cust['customer_id']] = "High"  # high condition
-        if total(cust) < 1500 and total(cust) > 500:
+        elif total(cust) <= 1500 and total(cust) >= 500:
             category[cust['customer_id']] = "Medium" # medium condition
-        if total(cust) < 500:
+        elif total(cust) < 500:
             category[cust['customer_id']] = "low" # low condition
 
     return category # return Dictionary
@@ -44,7 +44,9 @@ def main():
         {'customer_id': 1, 'electronics': 200, 'furniture': 700, 'stationery': 100},
         {'customer_id': 2, 'electronics': 300, 'furniture': 300, 'stationery': 300},
         {'customer_id': 3, 'electronics': 100, 'furniture': 100, 'stationery': 50},
-        {'customer_id': 4, 'electronics': 800, 'furniture': 1000, 'stationery': 200}
+        {'customer_id': 4, 'electronics': 800, 'furniture': 1000, 'stationery': 200},
+        {'customer_id': 5, 'electronics': 200, 'furniture': 200, 'stationery': 100},
+        {'customer_id': 6, 'electronics': 500, 'furniture': 500, 'stationery': 500}
     ]
 
     print(segment_customers(purchases))
