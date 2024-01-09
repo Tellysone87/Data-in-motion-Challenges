567b11a975fe19c0c13ee7128a3c6c3c33fd526b
Tellysone87 <walterdtrt@aol.com>
2024-01-09 12:06:16 -0500

    Completed challenge 5

--- ProCodeReviewChallenges/Challenge5.py
+++ ProCodeReviewChallenges/Challenge5.py
@@ -6,6 +6,6 @@
 # https://d-i-motion.com/lessons/challenge-5/
 
 def convert_to_int(input_list):
-    return [int(i) for i in input_list]
+    return [int(i) for i in input_list if i.isnumeric()] ## added if statement to prevent the error occuring. 
 
 print(convert_to_int(['1', '2', 'three']))
